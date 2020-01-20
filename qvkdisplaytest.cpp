56f0fb5c1fba4f393717da8852e59803a312f579
Laszlo Agocs <laszlo.agocs@qt.io>
2020-01-20 12:22:10 +0100

    Create a surface

--- qvkdisplaytest.cpp
+++ qvkdisplaytest.cpp
@@ -9,7 +9,10 @@ PFN_vkGetPhysicalDeviceDisplayPlanePropertiesKHR pfnGetPhysicalDeviceDisplayPlan
 
 PFN_vkGetDisplayPlaneSupportedDisplaysKHR pfnGetDisplayPlaneSupportedDisplaysKHR;
 PFN_vkGetDisplayPlaneCapabilitiesKHR pfnGetDisplayPlaneCapabilitiesKHR;
+
 PFN_vkCreateDisplayPlaneSurfaceKHR pfnCreateDisplayPlaneSurfaceKHR;
+
+PFN_vkDestroySurfaceKHR pfnDestroySurfaceKHR;
 
 void resolveDisplayFuncs(QVulkanInstance *inst)
 {
@@ -24,8 +27,12 @@ void resolveDisplayFuncs(QVulkanInstance *inst)
         inst->getInstanceProcAddr("vkGetDisplayPlaneSupportedDisplaysKHR");
     pfnGetDisplayPlaneCapabilitiesKHR = (PFN_vkGetDisplayPlaneCapabilitiesKHR)
         inst->getInstanceProcAddr("vkGetDisplayPlaneCapabilitiesKHR");
+
     pfnCreateDisplayPlaneSurfaceKHR = (PFN_vkCreateDisplayPlaneSurfaceKHR)
         inst->getInstanceProcAddr("vkCreateDisplayPlaneSurfaceKHR");
+
+    pfnDestroySurfaceKHR = (PFN_vkDestroySurfaceKHR)
+        inst->getInstanceProcAddr("vkDestroySurfaceKHR");
 }
 
 int main(int argc, char **argv)
@@ -37,8 +44,11 @@ int main(int argc, char **argv)
     if (!inst.supportedExtensions().contains(QByteArrayLiteral("VK_KHR_display")))
         qFatal("VK_KHR_display is not supported");
 
+    // enable validation, if available
     inst.setLayers({ QByteArrayLiteral("VK_LAYER_LUNARG_standard_validation") });
-    inst.setExtensions({ QByteArrayLiteral("VK_KHR_display") });
+
+    // for now assume the platform plugin will do this for us
+    // inst.setExtensions({ QByteArrayLiteral("VK_KHR_display") });
 
     if (!inst.create())
         qFatal("Failed to create Vulkan instance: %d", inst.errorCode());
@@ -67,12 +77,21 @@ int main(int argc, char **argv)
     displayProps.resize(displayCount);
     pfnGetPhysicalDeviceDisplayPropertiesKHR(physDev, &displayCount, displayProps.data());
 
+    VkDisplayKHR display = VK_NULL_HANDLE;
+    VkDisplayModeKHR displayMode = VK_NULL_HANDLE;
+    uint32_t width = 0;
+    uint32_t height = 0;
+
     for (uint32_t i = 0; i < displayCount; ++i) {
         const VkDisplayPropertiesKHR &disp(displayProps[i]);
         qDebug("Display #%u:\n  display: %p\n  name: %s\n  dimensions: %ux%u\n  resolution: %ux%u",
                i, (void *) disp.display, disp.displayName,
                disp.physicalDimensions.width, disp.physicalDimensions.height,
                disp.physicalResolution.width, disp.physicalResolution.height);
+
+        // Just pick the first display and the first mode.
+        if (i == 0)
+            display = disp.display;
 
         uint32_t modeCount = 0;
         if (pfnGetDisplayModePropertiesKHR(physDev, disp.display, &modeCount, nullptr) != VK_SUCCESS) {
@@ -84,12 +103,20 @@ int main(int argc, char **argv)
         pfnGetDisplayModePropertiesKHR(physDev, disp.display, &modeCount, modeProps.data());
         for (uint32_t j = 0; j < modeCount; ++j) {
             const VkDisplayModePropertiesKHR &mode(modeProps[j]);
-            qDebug("  Mode #%u:\n  mode: %p\n  visibleRegion: %ux%u\n  refreshRate: %u",
+            qDebug("  Mode #%u:\n    mode: %p\n    visibleRegion: %ux%u\n    refreshRate: %u",
                    j, (void *) mode.displayMode,
                    mode.parameters.visibleRegion.width, mode.parameters.visibleRegion.height,
                    mode.parameters.refreshRate);
+            if (j == 0) {
+                displayMode = mode.displayMode;
+                width = mode.parameters.visibleRegion.width;
+                height = mode.parameters.visibleRegion.height;
+            }
         }
     }
+
+    if (display == VK_NULL_HANDLE || displayMode == VK_NULL_HANDLE)
+        qFatal("Failed to choose display and mode");
 
     uint32_t planeCount = 0;
     err = pfnGetPhysicalDeviceDisplayPlanePropertiesKHR(physDev, &planeCount, nullptr);
@@ -102,5 +129,65 @@ int main(int argc, char **argv)
     planeProps.resize(planeCount);
     pfnGetPhysicalDeviceDisplayPlanePropertiesKHR(physDev, &planeCount, planeProps.data());
 
+    uint32_t planeIndex = UINT_MAX;
+    for (uint32_t i = 0; i < planeCount; ++i) {
+        uint32_t supportedDisplayCount = 0;
+        err = pfnGetDisplayPlaneSupportedDisplaysKHR(physDev, i, &supportedDisplayCount, nullptr);
+        if (err != VK_SUCCESS)
+            qFatal("Failed to query supported displays for plane: %d", err);
+
+        QVarLengthArray<VkDisplayKHR, 4> supportedDisplays;
+        supportedDisplays.resize(supportedDisplayCount);
+        pfnGetDisplayPlaneSupportedDisplaysKHR(physDev, i, &supportedDisplayCount, supportedDisplays.data());
+        qDebug("Plane #%u supports %u displays, currently bound to display %p",
+               i, supportedDisplayCount, (void *) planeProps[i].currentDisplay);
+
+        VkDisplayPlaneCapabilitiesKHR caps;
+        err = pfnGetDisplayPlaneCapabilitiesKHR(physDev, displayMode, i, &caps);
+        if (err != VK_SUCCESS)
+            qFatal("Failed to query plane capabilities: %d", err);
+
+        qDebug("  supportedAlpha: %d (1=no, 2=global, 4=per pixel, 8=per pixel premul)\n"
+               "  minSrc=%d, %d %ux%u\n"
+               "  maxSrc=%d, %d %ux%u\n"
+               "  minDst=%d, %d %ux%u\n"
+               "  maxDst=%d, %d %ux%u\n",
+               int(caps.supportedAlpha),
+               caps.minSrcPosition.x, caps.minSrcPosition.y, caps.minSrcExtent.width, caps.minSrcExtent.height,
+               caps.maxSrcPosition.x, caps.maxSrcPosition.y, caps.maxSrcExtent.width, caps.maxSrcExtent.height,
+               caps.minDstPosition.x, caps.minDstPosition.y, caps.minDstExtent.width, caps.minDstExtent.height,
+               caps.maxDstPosition.x, caps.maxDstPosition.y, caps.maxDstExtent.width, caps.maxDstExtent.height);
+
+        // if the plane is not in use and supports our chosen display, use that plane
+        if (supportedDisplays.contains(display)
+            && (planeProps[i].currentDisplay == VK_NULL_HANDLE || planeProps[i].currentDisplay == display))
+        {
+            planeIndex = i;
+        }
+    }
+
+    if (planeIndex == UINT_MAX)
+        qFatal("Failed to find a suitable plane");
+
+    qDebug("Using plane #%u", planeIndex);
+
+    VkDisplaySurfaceCreateInfoKHR surfaceCreateInfo = {};
+    surfaceCreateInfo.sType = VK_STRUCTURE_TYPE_DISPLAY_SURFACE_CREATE_INFO_KHR;
+    surfaceCreateInfo.displayMode = displayMode;
+    surfaceCreateInfo.planeIndex = planeIndex;
+    surfaceCreateInfo.planeStackIndex = planeProps[planeIndex].currentStackIndex;
+    surfaceCreateInfo.transform = VK_SURFACE_TRANSFORM_INHERIT_BIT_KHR;
+    surfaceCreateInfo.globalAlpha = 1.0f;
+    surfaceCreateInfo.alphaMode = VK_DISPLAY_PLANE_ALPHA_OPAQUE_BIT_KHR;
+    surfaceCreateInfo.imageExtent = { width, height };
+
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+    err = pfnCreateDisplayPlaneSurfaceKHR(inst.vkInstance(), &surfaceCreateInfo, nullptr, &surface);
+    if (err != VK_SUCCESS || surface == VK_NULL_HANDLE)
+        qFatal("Failed to create surface: %d", err);
+
+    qDebug("Created surface %p", (void *) surface);
+
+    pfnDestroySurfaceKHR(inst.vkInstance(), surface, nullptr);
     return 0;
 }
